436082a8bdc6a2f169acaa1c92be0a429dbcaba6
silva guimaraes <silva__guimaraes@hotmail.com>
2023-07-10 14:14:50 -0300

    calcular indice reverso todas as vezes que o script rodar

--- indexer.py
+++ indexer.py
@@ -1,10 +1,11 @@
 import nltk
-import time
+import glob
 import math
 import os
 import requests
 import sys
 import json
+# import time
 # import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 
@@ -14,7 +15,7 @@ nltk.download('punkt')
 # atribui valores altos para termos que tenham relevacia para uma pagina
 # porem ao mesmo tempo nao aparecendo com frequencia em outras paginas.
 # termos relevantes sao termos que aparecem com frequencia em uma pagina.
-# termos que aparecem com frequencia em todas as paginas 
+# termos que aparecem com frequencia em todas as paginas
 # recebem valores baixos.
 # https://en.wikipedia.org/wiki/Tf%E2%80%93idf
 # é perfeito pra distinguir uma pagina da outra
@@ -49,40 +50,50 @@ class website():
     term_frequency = {}
     tf_idf = {}
     url = ""
+    shouldIndex = False
 
-    def __init__(self, url, term_frequency):
-        self.url = ""
+    def __init__(self, url, shouldIndex):
+        self.url = url
         self.tf_idf = {}
         self.term_frequency = {}
 
-        self.url = url
+        self.shouldIndex = shouldIndex
+
+    def __repr__(self):
+        return self.url
+
+    def calculate_term_freq(self):
+        ret = requests.get(self.url)
+
+        soup = BeautifulSoup(ret.content, 'html.parser')
+
+        tags = ['p', 'dd', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+        text = soup.findAll(tags)
+        text = [i.getText().lower() for i in text]
+        text = ' '.join(text)
+
+        tokens = nltk.word_tokenize(text)
+        # tokens = [ps.stem(i) for i in tokens]
+
+        count = {}
+        for i in tokens:
+            if i not in count:
+                count[i] = 0
+            count[i] += 1
+
+        term_frequency = {}
+        for k, v in count.items():
+            term_frequency[k] = v / len(tokens)
+
         self.term_frequency = term_frequency
 
 
-def foobar(url):
-    ret = requests.get(url)
-
-    soup = BeautifulSoup(ret.content, 'html.parser')
-
-    tags = ['p', 'dd', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-    text = soup.findAll(tags)
-    text = [i.getText().lower() for i in text]
-    text = ''.join(text)
-
-    tokens = nltk.word_tokenize(text)
-    # tokens = [ps.stem(i) for i in tokens]
-
-    count = {}
-    for i in tokens:
-        if i not in count:
-            count[i] = 0
-        count[i] += 1
-
-    term_frequency = {}
-    for k, v in count.items():
-        term_frequency[k] = v / len(tokens)
-
-    return website(url, term_frequency)
+def read_page(filename):
+    with open(filename) as file:
+        load = json.loads(file.read())
+        page = website(load['url'], False)
+        page.tf_idf = load['tf_idf']
+    return page
 
 
 urls = []
@@ -90,6 +101,7 @@ reverse_index = {}
 websites = []
 last_time = 0
 documents_number = 0
+reverse_index = {}
 
 if os.path.exists('urls.txt'):
     with open('urls.txt') as file:
@@ -101,20 +113,24 @@ if not urls:
     print('urls.txt?', file=sys.stderr)
     sys.exit(1)
 
-if os.path.exists('reverse_index.json'):
-    with open('reverse_index.json') as file:
-        load = json.loads(file.read())
-        reverse_index = load['reverse_index']
-        last_time = load['time']
-        documents_number = load['documents_number']
+if os.path.exists('index'):
+    for filename in glob.glob("index/*"):
+        if os.path.isfile(filename):
+            websites.append(read_page(filename))
+for page in websites:
+    for k, v in page.term_frequency.items():
+        if k not in reverse_index:
+            reverse_index[k] = []
+        reverse_index[k].append(page.url)
 
-documents_number = len(urls) + documents_number
+documents_number = len(urls) + len(websites)
 
 for url in urls:
     print("download", url)
-    processed_website = foobar(url)
-    websites.append(processed_website)
-    for k, v in processed_website.term_frequency.items():
+    page = website(url, True)
+    page.calculate_term_freq()
+    websites.append(page)
+    for k, v in page.term_frequency.items():
         if k not in reverse_index:
             reverse_index[k] = []
         reverse_index[k].append(url)
@@ -123,20 +139,14 @@ inverse_doc_frequency = {}
 for k, v in reverse_index.items():
     inverse_doc_frequency[k] = -(math.log(len(v) / documents_number))
 
+websites = [page for page in websites if page.shouldIndex]
+
 for page in websites:
     for k, term_freq in page.term_frequency.items():
         page.tf_idf[k] = inverse_doc_frequency[k] * term_freq
 
 if not os.path.exists('index'):
     os.makedirs('index')
-
-
-with open('reverse_index.json', 'w') as file:
-    file.write(json.dumps({
-        "time": time.time(),
-        "reverse_index": reverse_index,
-        "documents_number": documents_number,
-        }))
 
 for page in websites:
     print("save", page.url)
